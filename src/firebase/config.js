469ca5f0761b40c26031830427ce6d3b2af7c5da
kpose <ganihujude@gmail.com>
2020-12-09 01:43:23 -0800

    setup firebase and global styles

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,14 @@
 import Firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "",
-  databaseUrl: "",
-  //others from firebase
+  apiKey: "AIzaSyCxU8piznKceAhzQUK8AKLq1Locpp6E0Dw",
+  authDomain: "rocketchater.firebaseapp.com",
+  projectId: "rocketchater",
+  storageBucket: "rocketchater.appspot.com",
+  messagingSenderId: "380401739842",
+  appId: "1:380401739842:web:4904582311f777b58de5db",
+  measurementId: "G-E2652C26TJ",
+  databaseURL: "https://rocketchater-default-rtdb.firebaseio.com/",
 };
 
 export default Firebase.initializeApp(firebaseConfig);
