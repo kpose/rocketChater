0dfb30caa4e4385772c30d633a1be7cc65e567a5
kpose <ganihujude@gmail.com>
2020-12-09 03:47:44 -0800

    basic logout function

--- src/container/Dashboard/index.js
+++ src/container/Dashboard/index.js
@@ -1,7 +1,37 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { View, Text, Alert } from "react-native";
+import { SimpleLineIcons } from "@expo/vector-icons";
+import { color } from "../../utility";
 
-const Dashboard = () => {
+const Dashboard = ({ navigation }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <SimpleLineIcons
+          name="logout"
+          size={26}
+          color={color.WHITE}
+          style={{ right: 10 }}
+          onPress={() =>
+            Alert.alert(
+              "Logout",
+              "Are you sure to log out",
+              [
+                {
+                  text: "Yes",
+                  onPress: () => alert("user logged out"),
+                },
+                {
+                  text: "No",
+                },
+              ],
+              { cancelable: false }
+            )
+          }
+        />
+      ),
+    });
+  }, [navigation]);
   return (
     <View>
       <Text>Dashboard</Text>
