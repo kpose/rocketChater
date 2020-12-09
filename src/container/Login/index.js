ee40eab58d05ba801019d5b3228a15eb0f2fbd3a
kpose <ganihujude@gmail.com>
2020-12-09 11:16:58 -0800

    login validation

--- src/container/Login/index.js
+++ src/container/Login/index.js
@@ -1,5 +1,5 @@
 import { View } from "native-base";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView, Text } from "react-native";
 import { globalStyle, color } from "../../utility";
 import { InputField, Button } from "../../components";
@@ -8,6 +8,29 @@ import LottieView from "lottie-react-native";
 const animation = require("../../annimation/chatlogo.json");
 
 const Login = ({ navigation }) => {
+  const [credentials, setCredentials] = useState({
+    email,
+    password,
+  });
+
+  const { email, password } = credentials;
+
+  const handleOnChange = (name, value) => {
+    setCredentials({
+      ...credentials,
+      [name]: value,
+    });
+  };
+
+  onLoginPress = () => {
+    if (!email) {
+      alert("Email is required");
+    } else if (!password) {
+      alert("Please, enter a password");
+    } else {
+      alert(JSON.stringify(credentials));
+    }
+  };
   return (
     <SafeAreaView style={[globalStyle.flex1, { backgroundColor: color.BLACK }]}>
       <View style={[globalStyle.containerCentered]}>
@@ -21,9 +44,20 @@ const Login = ({ navigation }) => {
       </View>
 
       <View style={[globalStyle.flex2, globalStyle.sectionCentered]}>
-        <InputField placeholder="Enter email" />
-        <InputField placeholder="Enter password" />
-        <Button title="Login" />
+        <InputField
+          placeholder="Enter email"
+          value={email}
+          onChangeText={(text) => handleOnChange("email", text)}
+        />
+
+        <InputField
+          placeholder="Enter password"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(text) => handleOnChange("password", text)}
+        />
+
+        <Button title="Login" onPress={() => onLoginPress()} />
         <Text
           style={{
             fontSize: 28,
