95ff688c02725af09c8bd462661a2a2a0ec921ae
kpose <ganihujude@gmail.com>
2020-12-10 12:01:34 -0800

    chat screen

--- src/container/Chat/index.js
+++ src/container/Chat/index.js
@@ -1,10 +1,51 @@
-import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { View, Text, SafeAreaView, FlatList } from "react-native";
+import { globalStyle, color, appStyle } from "../../utility";
+import styles from "./styles";
+import { InputField } from "../../components";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Chat = ({ route, navigation }) => {
+  const { params } = route;
+  const { name, img, imgText, guestUserId, currentUserId } = params;
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: <Text>{name}</Text>,
+    });
+    return () => {
+      cleanup;
+    };
+  }, [navigation]);
   return (
-    <SafeAreaView>
-      <Text>Chat</Text>
+    <SafeAreaView style={[globalStyle.flex1, { backgroundColor: color.BLACK }]}>
+      <FlatList
+        inverted
+        data={[1, 2, 3, 4]}
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={({ item }) => <Text>{name}</Text>}
+      />
+
+      {/* send message */}
+      <View style={styles.sendMessageContainer}>
+        <InputField
+          placeholder="Type message here"
+          numberOfLines={10}
+          inputStyle={styles.input}
+        />
+
+        <View style={styles.sendBtnContainer}>
+          <MaterialCommunityIcons
+            name="camera"
+            color={color.WHITE}
+            size={appStyle.fieldHeight}
+          />
+          <MaterialCommunityIcons
+            name="send-circle"
+            color={color.WHITE}
+            size={appStyle.fieldHeight}
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
